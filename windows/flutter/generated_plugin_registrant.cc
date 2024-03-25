156c886978658ebd86f5abed90422a3dbf1c11d8
Tiago Dinis <tiagodinis33@proton.me>
2024-03-25 19:51:36 +0000

    deps: add dart_discord_rpc

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <dart_discord_rpc/dart_discord_rpc_plugin.h>
 #include <objectbox_flutter_libs/objectbox_flutter_libs_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  DartDiscordRpcPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DartDiscordRpcPlugin"));
   ObjectboxFlutterLibsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("ObjectboxFlutterLibsPlugin"));
 }
